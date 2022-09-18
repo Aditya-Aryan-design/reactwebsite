7ef9110093f7899b9e81487bed94f51f7dee60c4
Aditya-Aryan-design <113804917+Aditya-Aryan-design@users.noreply.github.com>
2022-09-18 16:56:30 +0530

    Updates

---
+++ precache-manifest.c1ca92a8b7ab9f2a2b0f92666fd39019.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "606435bc6a0f8454f6b1",
+    "url": "/reactwebsite/static/css/main.1af24ae9.chunk.css"
+  },
+  {
+    "revision": "606435bc6a0f8454f6b1",
+    "url": "/reactwebsite/static/js/main.606435bc.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/reactwebsite/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/reactwebsite/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "6b05b4e26d64a4630af2",
+    "url": "/reactwebsite/static/js/runtime~main.6b05b4e2.js"
+  },
+  {
+    "revision": "b770841954ebda6cb04b4aa5a4ec6237",
+    "url": "/reactwebsite/index.html"
+  }
+];
